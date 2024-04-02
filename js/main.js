f64fc81ab34eb7359f475f9e04d92f97a2a3f94e
annahico <annahico@gmail.com>
2024-04-02 19:38:52 +0200

    creando codigo para validar DNI

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+function validarDNI(number, letter) {
+    const letters = ['T,R,W,A,G,M,Y,F,P,D,X,B,N,J,Z,S,Q,V,H,L,C,K,E,T',];
+    
+    // Calcular el módulo del número del DNI entre 23
+    const index = number % 23;
+    
+    // Obtener la letra correspondiente del array
+    const calculatedLetter = letter [index];
+    
+    // Comparar con la letra proporcionada
 
+    calculetedLetter = letter.toUpperCase();
+//     if (letraCalculada === letra.toUpperCase()) {
+//         return true;
+//     } else {
+//         return false;
+//     }
 
-console.log()
+return letter == calculatedLetter;
+
+}
+
+const result = validarDNI(123456789, "H");
+console.log(result);
